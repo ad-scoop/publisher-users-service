395670d5633683b1963c50ff8fb25afd9b5b4543
= <=>
2017-01-23 21:36:04 +0100

    added methods to interfaces

--- src/main/java/com/adscoop/services/CampaginService.java
+++ src/main/java/com/adscoop/services/CampaginService.java
@@ -1,7 +1,24 @@
 package com.adscoop.services;
 
+import com.adscoop.entiites.BannerNode;
+import com.adscoop.entiites.Campagin;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by thokle on 23/01/2017.
  */
 public interface CampaginService {
+
+
+
+    Campagin createCampagin(Campagin campagin);
+
+    boolean DeleteCampagin(Campagin campagin);
+
+    boolean UpdateCampagin(Campagin campagin);
+
+
+
 }
