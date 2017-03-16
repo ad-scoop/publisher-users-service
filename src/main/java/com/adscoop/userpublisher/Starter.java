4b8187bcde3e50d99c6e20b90bbbda517c03601b
= <=>
2017-03-16 20:30:50 +0100

    added metrics

--- src/main/java/com/adscoop/userpublisher/Starter.java
+++ src/main/java/com/adscoop/userpublisher/Starter.java
@@ -8,7 +8,6 @@ import com.adscoop.userpublisher.exceptions.ServerErrorHandler;
 import com.adscoop.userpublisher.handlers.CORSHandler;
 import com.adscoop.userpublisher.modules.Config;
 import com.adscoop.userpublisher.modules.ServiceCommonConfigModule;
-
 import ratpack.dropwizard.metrics.DropwizardMetricsConfig;
 import ratpack.dropwizard.metrics.DropwizardMetricsModule;
 import ratpack.dropwizard.metrics.MetricsWebsocketBroadcastHandler;
@@ -18,6 +17,7 @@ import ratpack.rx.RxRatpack;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,29 +26,40 @@ import java.util.concurrent.TimeUnit;
 public class Starter {
 
 
-    public static void main(String ... args) throws Exception {
+    public static void main(String... args) throws Exception {
         RxRatpack.initialize();
 
         RatpackServer.start(ratpackServerSpec -> ratpackServerSpec
-        		.serverConfig(sfb -> sfb.baseDir(BaseDir.find())
-        				.props("ratpack.properties")
-        				.yaml("database.yaml")
-        				.require("/db", Config.class).require("/metrics", DropwizardMetricsConfig.class)
-        				.env().development(false)
-        				.sysProps()
-        				.build())
+                .serverConfig(sfb -> sfb.baseDir(BaseDir.find())
+                        .props("ratpack.properties")
+                        .yaml("database.yaml")
+                        .require("/db", Config.class).require("/metrics", DropwizardMetricsConfig.class)
+                        .env().development(false)
+                        .sysProps()
+                        .build())
                 .registry(Guice.registry(bindingsSpec -> bindingsSpec
-                		.module(ConfigModule.class).module(DropwizardMetricsModule.class, d -> {
-							d.getGraphite().get().prefix("publisher-service").durationUnit(TimeUnit.SECONDS);
+                        .module(ConfigModule.class).module(DropwizardMetricsModule.class, d -> {
+                            d.getGraphite(   ).get().prefix("publisher-service").durationUnit(TimeUnit.SECONDS);
+                            d.jmx(jmxConfig -> {
+                                jmxConfig.enable(true);
+                            });
+                            d.console(  consoleConfig -> {
+                                consoleConfig.enable(true);
+                                consoleConfig.reporterInterval(Duration.ofSeconds(2));
+                            });
 
-
-							})
-                		.module(ServiceCommonConfigModule.class).bind(PublisherUserException.class).bind(ServerErrorHandler.class))).
+                            d.webSocket( websocketConfig -> {
+                               websocketConfig.reporterInterval(Duration.ofSeconds(2));
+                            });
+                            d.console();
+                            d.jmx();
+                        })
+                        .module(ServiceCommonConfigModule.class).bind(PublisherUserException.class).bind(ServerErrorHandler.class))).
                         handlers(chain -> chain
-                        		.all(CORSHandler.class)
-                        		.prefix("banners", BannerNodeChain.class)
-                        		.prefix("campagin", CampaginChain.class).prefix("admin",
-										admin -> admin.get("health-check/:name", new HealthCheckHandler()).get("metric-report", new MetricsWebsocketBroadcastHandler()))));
+                                .all(CORSHandler.class)
+                                .prefix("banners", BannerNodeChain.class)
+                                .prefix("campagin", CampaginChain.class).prefix("admin",
+                                        admin -> admin.get("health-check/:name", new HealthCheckHandler()).get("metric-report", new MetricsWebsocketBroadcastHandler()))));
     }
-    
+
 }
