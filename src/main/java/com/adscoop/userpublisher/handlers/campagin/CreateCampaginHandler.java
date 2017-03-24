18e885df0a6409e5bc1ec4e0e3c80c9f15c1d204
= <=>
2017-03-24 06:38:32 +0100

    refactored query

--- src/main/java/com/adscoop/userpublisher/handlers/campagin/CreateCampaginHandler.java
+++ src/main/java/com/adscoop/userpublisher/handlers/campagin/CreateCampaginHandler.java
@@ -28,11 +28,11 @@ public class CreateCampaginHandler implements Handler {
 	public void handle(Context ctx) throws Exception {
 		this.extractUser.handle(ctx, user -> { 
 			ctx.parse(fromJson(Campagin.class)).then(campagin -> {
-
-				campagin.getBanners().forEach(banner -> campagin.addBanner(banner));
-				user.addCampagin(campagin);
-				userSevice.saveOrUpate(user);
-				ctx.render(json("created ok"));
+				ctx.parse(fromJson(Campagin.class)).then(campagin -> {
+					user.addCampagin(campagin);
+					userSevice.saveOrUpate(user);
+					ctx.render(json("created ok"));
+				});
 			});
 		}); 
 	}
